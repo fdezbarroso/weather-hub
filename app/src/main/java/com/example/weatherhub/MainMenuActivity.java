daff2c7b3d778f79d3cbf8a81656ebdf54e10ec6
PedroMej <pmejiaskudelka@gmail.com>
2023-01-13 16:01:19 +0100

    gps_and_nofications

--- app/src/main/java/com/example/weatherhub/MainMenuActivity.java
+++ app/src/main/java/com/example/weatherhub/MainMenuActivity.java
@@ -1,27 +1,84 @@
 package com.example.weatherhub;
 
+
+import static android.content.ContentValues.TAG;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Looper;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.core.content.ContextCompat;
 
-import user.User;
-import user.UserFunctions;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MainMenuActivity extends AppCompatActivity {
+    TextView city, country;
+    private final static int REQUEST_CODE = 100;
+    LocationManager locationManager;
 
-    UserFunctions userFunctions;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getSupportActionBar().hide();
         setContentView(R.layout.main_menu);
+
+        city = findViewById(R.id.textView1);
+        country = findViewById(R.id.textView14);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainMenuActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+        } else {
+            Location loc = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            Geocoder gcd = new Geocoder(getBaseContext(), Locale.getDefault());
+            List<Address> addresses;
+            try {
+                addresses = gcd.getFromLocation(loc.getLatitude(), loc.getLongitude(), 1);
+                if (addresses.size() > 0) {
+                    city.setText(addresses.get(0).getLocality());
+                    country.setText(addresses.get(0).getCountryName());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
         //Cierra la sesión del usuario
         Button logout = findViewById(R.id.button3);
         logout.setOnClickListener(new View.OnClickListener() {
@@ -34,5 +91,35 @@ public class MainMenuActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("My Notification", "My Notification", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+
+            NotificationCompat.Builder mBuilder =
+                    new NotificationCompat.Builder(this, channel.getId())
+                            .setSmallIcon(R.drawable.icon_weather_notify)
+                            .setContentTitle("My notification")
+                            .setContentText(city.getText().toString() + ", " + country.getText().toString());
+
+            NotificationManagerCompat managerCompat = NotificationManagerCompat.from(MainMenuActivity.this);
+            managerCompat.notify(1, mBuilder.build());
+
+            Intent resultIntent = new Intent(this, MainMenuActivity.class);
+            resultIntent.setAction(Intent.ACTION_MAIN);
+            resultIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,
+                    resultIntent, 0);
+            mBuilder.setContentIntent(pendingIntent);
+            NotificationManager mNotificationManager =
+                    (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            mNotificationManager.notify(1, mBuilder.build());
+        }
     }
 }
